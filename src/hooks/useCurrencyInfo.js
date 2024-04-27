01e8a685dfa395f4b9db7be8c74c6a78e726d721
Hrshit5616 <34878031+Hrshit5616@users.noreply.github.com>
2024-04-27 18:31:23 +0530

    currency project

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+
+import { useEffect, useState } from "react";
+
+function useCurrencyinfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+            
+    }, [currency]);
+    console.log(data);
+
+    return data;
+}
+export default useCurrencyinfo;
